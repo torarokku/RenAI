7dd95c83fcab9f25647726b8fc9509d5a4b2c951
hayato ito <yitengsaren97@gmail.com>
2026-07-02 10:18:20 +0900

    アクセスできるページをすべて作りました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'conversations/index'
+  get 'sessions/new'
+  get 'users/new'
+  get 'home/index'
+  root "home#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  resources :conversations, only: [:index, :create]
 end

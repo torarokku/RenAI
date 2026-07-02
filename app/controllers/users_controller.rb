4d60a43b0e58009014d70208e3b1baee3b420e29
radish <r7_ueda@icc.core.ac.jp>
2026-07-02 13:48:13 +0900

    ユーザー登録が完了いたしました！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to home_index_path, notice:"ユーザー登録が完了しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 end

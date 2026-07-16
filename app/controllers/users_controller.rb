728d656a9630989b0e18edaa66e5238dac185ab0
hayato ito <yitengsaren97@gmail.com>
2026-07-16 15:42:17 +0900

    新規登録時に自動でログインをするようにしました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to home_index_path, notice:"ユーザー登録が完了しました。"
     else
       render :new, status: :unprocessable_entity
